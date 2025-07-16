8b4b2a3cf05b6b4951f8fa089a8b4b9f656ef499
shravan kumar <131240954+shravan7local@users.noreply.github.com>
2025-07-16 21:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def contact():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT
+    app.run(host='0.0.0.0', port=port, debug=True)  # Bind to 0.0.0.0
